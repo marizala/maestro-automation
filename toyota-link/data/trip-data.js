2388116c095ece007c5852747128b8b3c4d85f8f
Michael Angelo Arizala <michaelangelo.arizala@gmail.com>
2023-09-13 17:33:54 +1000

    Data set

---
+++ toyota-link/data/trip-data.js
@@ -0,0 +1,9 @@
+'### DATA: Setters used in functions ###'
+output.SET_tripname = 'New Trip';
+output.SET_place = '';
+
+
+'DATA'
+output.tripname1 = "Trip 1"
+output.place1 = 'Queen Victoria Market';
+output.place2 = 'Melbourne Zoo'
